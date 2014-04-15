9881d6c9fb82276c33a2d67a04907a06f52d758b
Seth-Johnson <sethj0438@gmail.com>
2014-04-15 11:29:56 -0700

    tidy up and add comments

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ var answers = [];
 var currentCard = 0;
 var previousCard;
 // we'll be doing a lot of sliding, let's make it easier
-hideCard = { "direction": "left", "mode": "hide" }; 
-showCard = { "direction": "right", "mode": "show"};
+hideCard = {
+    "direction": "left",
+    "mode": "hide"
+};
+showCard = {
+    "direction": "right",
+    "mode": "show"
+};
 // to keep track of whether we're showing a question or an answer
 var isQ = true;
 // because typing document.getElementByClassName everytime isn't worth it
@@ -19,19 +25,19 @@ $('#card').hide();
 
 
 // define flipping here
-$('#card').click(function() {
-  $('#card').flip({
-      direction: 'tb',
-      speed: 350,
-      color: 'lightgrey',
-      content: testQuestion()
-  });
+$('#card').click(function () {
+    $('#card').flip({
+        direction: 'tb',
+        speed: 350,
+        color: 'lightgrey',
+        content: testQuestion()
+    });
 });
 // ugh; jquery, jquery everwhere.
 
 // next button click handler
 // for now just show cards randomly, later we'll offer an option for showing them in order
-$('#nextCard').click(function() {
+$('#nextCard').click(function () {
     previousCard = currentCard;
     currentCard = getRandomCard();
     if ($('#card').is(':hidden')) {
@@ -39,13 +45,12 @@ $('#nextCard').click(function() {
         isQ = false;
         $('#card').show().html(testQuestion());
         $('#placeHolder').remove();
-}
-     else {
-    // false 'cause we want a question when it slides
-    isQ = false;
-    $('.card').effect('slide', hideCard, 750).html(testQuestion()).effect('slide', showCard, 500);
-    
-}
+    } else {
+        // false 'cause we want a question when it slides
+        isQ = false;
+        $('.card').effect('slide', hideCard, 750).html(testQuestion()).effect('slide', showCard, 500);
+
+    }
 });
 
 
@@ -57,44 +62,47 @@ var getRandomCard = function () {
     return randomNum;
 };
 
-// figure out if we're showing the question or the answer, then chnage it
-var testQuestion = function() {
+// figure out if we're showing the question or the answer, then change it
+var testQuestion = function () {
     if (isQ) {
         isQ = false;
         return '<h1>' + answers[currentCard] + '</h1>';
-    }
-    else {
+    } else {
         isQ = true;
         return '<h1>' + questions[currentCard] + '</h1>';
     }
 };
 
-$('#saveCards').click(function() {
-        for (i=0; i<questionInputs.length; i++) {
-            if (questionInputs[i].value.length == 0) {
-                continue;
+// handle updating the cards
+$('#saveCards').click(function () {
+    // loop through the question inputs
+    for (i = 0; i < questionInputs.length; i++) {
+        // check if a question was defined in the current input
+        if (questionInputs[i].value.length === 0) {
+            // if not, skip it
+            continue;
+        // if the question was defined push it to the questions array
+        } else {
+            alert('The value is ' + questionInputs[i].value);
+            questions.push(questionInputs[i].value);
+            // check if an answer was defined for the question. If not, issue a warning
+            if (answerInputs[i].value.length === 0) {
+                // we don't want more than one warning!
+                if (document.contains(document.getElementById('warning'))) {
+                    continue;
+                // show the warning
+                } else {
+                    $('#editCards').append('<div class="alert alert-warning alert-dismissable" id="warning"> <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button> <strong>Warning!</strong> One or more questions was defined but left without an answer</div>');
+                    // We have a question and they have a warning, so lets pretend they did it on purpose.
+                    answers.push('An answer wasn\'t given :( ');
+                }
+            // if we're all good, add the answer to the answer array
+            } else {
+                answers.push(answerInputs[i].value);
             }
-            else {
-                alert('The value is ' + questionInputs[i].value);
-                questions.push(questionInputs[i].value);
-                if (answerInputs[i].value.length === 0) {
-                    if (document.contains(document.getElementById('warning'))) {
-                        continue;
-                    }
-                    else {
-                        $('#editCards').append('<div class="alert alert-warning alert-dismissable" id="warning"> <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button> <strong>Warning!</strong> One or more questions was defined but left without an answer</div>');
-                        answers.push('An answer wasn\'t given :( ')
-                    }
-                }
-                else {
-                    answers.push(answerInputs[i].value);
-                }
-                    
-            
+
+
+        }
     }
-}
-  
-  });
 
-
-
+});
